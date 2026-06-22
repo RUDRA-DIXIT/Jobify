9679e20259784e41822ec3cc165de7bedc2ec095
RUDRA-DIXIT <work.rudradixit@gmail.com>
2026-06-22 09:22:35 +0530

    Add middleware and centralized error handling

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,8 @@ const morgan= require('morgan');
 const bodyparser= require('body-parser');
 require('dotenv/config');
 var cors = require('cors');
+const cookieParser= require('cookie-parser');
+const errorHandler= require('./middleware/error');
 
 //Database connection
 mongoose.connect(process.env.DATABASE,{
@@ -18,6 +20,17 @@ mongoose.connect(process.env.DATABASE,{
     console.log('Database connection failed');
     console.log(err);
 });
+
+//middlewares
+app.use(morgarn('dev'));
+app.use(bodyparser.json({limit: '5mb'}));
+app.use(bodyparser.urlencoded({limit: '5mb', extended: true}));
+app.use(cookieParser());
+app.use(cors());
+
+// error middleqware
+app.use(errorHandler);
+
 //port
 const port= process.env.PORT || 9000; 
 app.listen(port,()=>{
